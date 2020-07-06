fc22fa09ea853d8aabc39f57a2aafb9b8b1df3d3
Cyrus Yip <cyruscmyip1@gmail.com>
2020-07-05 21:33:34 -0700

    game finished

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Player : MonoBehaviour
+{
+    public GameObject livesScreen;
+    public TextMeshProUGUI livesText;
+    public GameObject endScreen;
+    public TextMeshProUGUI statusText;
+    public TextMeshProUGUI infoText;
+    public KeyCode keyUp;
+    public KeyCode keyLeft;
+    public KeyCode keyDown;
+    public KeyCode keyRight;
+    private float speed = 0.6f;
+    private int lives = 100;
+    private bool gameEnd = false;
+    private float timeLeft = 24.3f;
+    void EndGame(bool won)
+    {
+        gameEnd = true;
+        livesScreen.SetActive(false);
+        endScreen.SetActive(true);
+        statusText.text = "You " + (won ? "won!" : "lost.");
+        infoText.text = "You " + (won ? "won with " + lives.ToString() + " lives!" : "lost with " + timeLeft.ToString() + "s remaining.");
+    }
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft <= 0 && !gameEnd)
+        {
+            EndGame(true);
+        }
+    }
+    void FixedUpdate()
+    {
+        if (Input.GetKey(keyUp))
+            GetComponent<Rigidbody>().velocity += new Vector3(speed, 0, 0);
+        if (Input.GetKey(keyDown))
+            GetComponent<Rigidbody>().velocity -= new Vector3(speed, 0, 0);
+        if (Input.GetKey(keyLeft))
+            GetComponent<Rigidbody>().velocity += new Vector3(0, 0, speed);
+        if (Input.GetKey(keyRight))
+            GetComponent<Rigidbody>().velocity -= new Vector3(0, 0, speed);
+    }
+    void OnParticleCollision(GameObject other)
+    {
+        lives -= 1;
+        livesText.text = lives.ToString();
+        if (lives == 0 && !gameEnd)
+        {
+            EndGame(false);
+        }
+    }
+}
---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    public void mainMenu() {
+        SceneManager.LoadScene(0);
+    }
+    public void game() {
+        SceneManager.LoadScene(1);
+    }
+}
